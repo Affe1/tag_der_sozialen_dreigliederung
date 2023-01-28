f1bbc768607c870e06a2fde090dc60ba7e0fd772
dreigliederung <110256509+dreigliederung@users.noreply.github.com>
2023-01-28 13:21:33 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+
+let sel = document.getElementById('slct');
+let down = document.getElementById('down');
+sel.addEventListener ("change", function () {
+
+    if(this.value == "englisch")
+    {
+        down.href ='plakat(eng).pdf'
+    }
+    if(this.value == "deutsch")
+    {
+        down.href ='plakat.pdf'
+    }
+    if(this.value == "russisch")
+    {
+        down.href ='plakat(rus).pdf'
+    }
+ 
+});
